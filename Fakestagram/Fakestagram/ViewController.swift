c8a5570bd997a9d626e866a5555ad681c37165a8
Luis Mauricio Esparza Vázquez <schockarum.9503@gmail.com>
2019-03-30 10:46:39 -0600

    Update .gitignore

---
+++ Fakestagram/Fakestagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fakestagram
+//
+//  Created by Luis Mauricio Esparza Vazquez on 3/30/19.
+//  Copyright © 2019 Schock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
